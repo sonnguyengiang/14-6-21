a53e796745525819903f718b5d61f2860a51c397
sonnguyengiang <sonstarx2212@gmail.com>
2021-06-14 17:59:25 +0700

    up

---
+++ test/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        class vet {
+            public void giveshort(Animal){
+
+            }
+        }
+        class Animal{
+            public Animal(){
+                System.out.println("avs");
+            }
+        }
+    }
+}
